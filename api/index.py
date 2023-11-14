4426a8980302cbfaddcb9d14a4aaae10be836707
Parjanya Roy <parjanyaroy@gmail.com>
2023-11-14 13:57:30 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler
-class handler(BaseHTTPRequestHandler)
-  def do_GET(self)
+class handler(BaseHTTPRequestHandler):
+  def do_GET(self):
     self.send_response(200)
     self.end_headers()
     self.wfile.write(self.headers.get('x-forwarded-for').encode())
